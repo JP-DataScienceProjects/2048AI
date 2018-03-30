bf48739eae935867d369e499554d9ec2f1595e99
pazzelli <john.pazzelli@gmail.com>
2018-03-30 20:49:02 +0000

    Updated training scripts

--- run_mini_train.sh
+++ run_mini_train.sh
@@ -4,6 +4,6 @@
 lr=0.0007
 max_tile=256
 
-python3 tfmodel.py --train 100 --max_tile=$max_tile --debug --learning_rate=$lr --suppress_charts
+python3 tfmodel.py --train 1000 --max_tile=$max_tile --debug --learning_rate=$lr --suppress_charts
 #python3 tfmodel.py --train 100 --max_tile 256 --debug --no_save --learning_rate=0.0007 --max_epsilon=0.0 --min_epsilon=0.0
 #python3 -m cProfile  tfmodel.py --train 10 --max_tile 256 --debug --suppress_charts --learning_rate=0.0007 > result.txt
--- run_training.sh
+++ run_training.sh
@@ -3,9 +3,13 @@
 
 lr=0.0007
 max_tile=256
+min_epsilon=0.5
 
-python3 tfmodel.py --train 10000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr
-python3 tfmodel.py --train 10000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr
-python3 tfmodel.py --train 10000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr
-python3 tfmodel.py --train 10000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr
-python3 tfmodel.py --train 100000 --max_tile=$max_tile --debug --learning_rate=$lr
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon
+#python3 tfmodel.py --train 200000 --max_tile=$max_tile --debug --learning_rate=$lr
