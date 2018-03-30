531bb93091797452f946078ff0f7023b659f2a4f
pazzelli <john.pazzelli@gmail.com>
2018-03-30 18:36:21 +0000

    Updated setup script

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y python3-pip
+sudo apt-get install python3-tk
+sudo apt install git-all
+
+sudo pip3 install --upgrade numpy matplotlib tensorflow
+
+git config --global user.email "john.pazzelli@gmail.com"
+git config --global user.name "pazzelli"
+
