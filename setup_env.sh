9d3388374bb0926b63b25cf4cb7ca0d1dec0e401
pazzelli <john.pazzelli@gmail.com>
2018-03-30 19:20:09 +0000

    Added h5py get

--- setup_env.sh
+++ setup_env.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python3-pip
 sudo apt-get install python3-tk
 sudo apt install git-all
 
-sudo pip3 install --upgrade numpy matplotlib tensorflow
+sudo pip3 install --upgrade numpy h5py matplotlib tensorflow
 
 git config --global user.email "john.pazzelli@gmail.com"
 git config --global user.name "pazzelli"
