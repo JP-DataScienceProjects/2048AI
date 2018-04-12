0b0fd34aa4a685fd7e42fd8b9bd0b5c4c6574c2c
pazzelli <john.pazzelli@gmail.com>
2018-04-12 22:45:35 +0000

    Updated weights after more training

--- run_training.sh
+++ run_training.sh
@@ -7,7 +7,7 @@ min_epsilon=0.1
 max_epsilon=1.0
 
 python3 main.py --train 10001 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
-python3 main.py --train 10002 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
+#python3 main.py --train 10002 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
 #python3 main.py --train 200002 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
 #python3 main.py --train 200003 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
 #python3 main.py --train 200004 --max_tile=$max_tile --debug --suppress_charts --learning_rate=$lr --min_epsilon=$min_epsilon --max_epsilon=$max_epsilon
